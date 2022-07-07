a67151870249fed8a27ef2bc2e6a16528f868340
Your Name <you@example.com>
2022-07-07 01:24:50 +0100

    first commit

---
+++ src/main/java/com/frida/authuser/enums/UserStatus.java
@@ -0,0 +1,6 @@
+package com.frida.authuser.enums;
+
+public enum UserStatus {
+    ACTIVE,
+    BLOCKED;
+}
---
+++ src/main/java/com/frida/authuser/enums/UserType.java
@@ -0,0 +1,7 @@
+package com.frida.authuser.enums;
+
+public enum UserType {
+    ADMIN,
+    STUDENT,
+    INSTRUCTOR;
+}
